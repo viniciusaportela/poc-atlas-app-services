e685afaae88704ca132e32e30021cc72ab4ccc8d
viniciusaportela <vinicius.portela.stm@gmail.com>
2023-10-05 22:02:45 -0300

    feat: initial commit

---
+++ packages/poc-atlas-app-services/src/App.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
